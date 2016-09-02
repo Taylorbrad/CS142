74b0236f3577f73754fa6d361a181da6bd1b3de7
Taylor Bradshaw <quiero_videojuegos@hotmail.com>
2016-09-02 15:43:22 -0600

    Update lab 1.cpp

--- lab 1.cpp
+++ lab 1.cpp
@@ -3,5 +3,8 @@ using namespace std;
 
 int main()
 {
-
+  cout << "\t\t\t\t\tBasic BYU Trivia\n";
+  cout << "\t\t\tQuestions\t\t\t\tAnswers\n\n";
+  cout << "What was the original name of BYU?\t\t\t"
+  
 }
