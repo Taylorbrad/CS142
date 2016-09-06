5d4ec81343c963a7047b2597f6f1882bd883d76e
Taylor Bradshaw <quiero_videojuegos@hotmail.com>
2016-09-06 14:52:51 -0600

    Update lab 1.cpp

--- lab 1.cpp
+++ lab 1.cpp
@@ -3,8 +3,15 @@ using namespace std;
 
 int main()
 {
-  cout << "\t\t\t\t\tBasic BYU Trivia\n";
-  cout << "\t\t\tQuestions\t\t\t\tAnswers\n\n";
-  cout << "What was the original name of BYU?\t\t\t"
-  
+  cout << "\t\t\t\tBasic BYU Trivia\n";
+  cout << "\t\tQuestions\t\t\t\tAnswers\n\n";
+  cout << "What was the original name of BYU?\t\tBrigham Young Academy\n";
+  cout << "When was BYA established?\t\t\t1875\n";
+  cout << "Who was the first \'permanent\' principal of BYA? Karl Maeser\n";
+  cout << "When did BYA become BYU?\t\t\t1903\n";
+  cout << "To what sports conference do we belong?\t\tIndependent (Football)\n";
+  cout << "When did BYU win the national football title?\t1984\n";
+  cout << "who won the Heisman Trophy in 1990?\t\tTy Detmer\n";
+  system("pause");
+  return 0;
 }
