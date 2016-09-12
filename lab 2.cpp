bacafaad4f1e2af85146e40ea1d9be9a74f02aff
Taylor Bradshaw <quiero_videojuegos@hotmail.com>
2016-09-12 15:36:29 -0600

    Update lab 2.cpp

--- lab 2.cpp
+++ lab 2.cpp
@@ -6,13 +6,9 @@ using namespace std;
 /*
 Taylor Bradshaw, Section _, taylor.bradshaw0250@gmail.com
 READY TO BE GRADED
-
 Purpose: This is a program which calculates the total bill for a group of people going to a pizza place.
-
 Inputs: The amount of people eating and the tip percentage.
-
 Outputs: The area that each person can eat, how many of each type of pizza, and the total bill.
-
 Test Case One:
 Test Case Two:
 Test Case Three:
@@ -20,13 +16,18 @@ Test Case Three:
 
 int main()
 {
-	const double SMALL_DIAMETER = 12;
-	const double MEDIUM_DIAMETER = 16;
-	const double LARGE_DIAMETER = 20;
+	const double SMALL_RADIUS = 6;
+	const double MEDIUM_RADIUS = 8;
+	const double LARGE_RADIUS = 10;
 	const double SMALL_PRICE = 7.28;
 	const double MEDIUM_PRICE = 11.48;
 	const double LARGE_PRICE = 14.68;
 	const double PI = 3.14159;
+	const int SQUARE = 2;
+	const int CONVERT_TO_PERCENTAGE = 100;
+	const int PEOPLE_PER_LARGE = 7;
+	const int PEOPLE_PER_MEDIUM = 3;
+	const int TO_NEAREST_DOLLAR = 0;
 	int numOfGuests = 0;
 	int numOfGuestsTemp = 0;
 	int largePizzas = 0;
@@ -48,17 +49,19 @@ int main()
 	cin >> numOfGuests;
 
 	numOfGuestsTemp = numOfGuests;
-	largePizzas = numOfGuestsTemp / 7;
-	numOfGuestsTemp -= (largePizzas * 7);
-	mediumPizzas = numOfGuestsTemp / 3;
-	numOfGuestsTemp -= (mediumPizzas * 3);
+	largePizzas = numOfGuestsTemp / PEOPLE_PER_LARGE;
+	numOfGuestsTemp -= (largePizzas * PEOPLE_PER_LARGE);
+	mediumPizzas = numOfGuestsTemp / PEOPLE_PER_MEDIUM;
+	numOfGuestsTemp -= (mediumPizzas * PEOPLE_PER_MEDIUM);
 	smallPizzas = numOfGuestsTemp;
 
 	cout << "\nLarge: " << largePizzas << "\nMedium: " << mediumPizzas << "\nSmall: " << smallPizzas;
-
-	smallArea = smallPizzas * (PI * pow((SMALL_DIAMETER / 2), 2));
-	mediumArea = mediumPizzas * (PI * pow((MEDIUM_DIAMETER / 2), 2));
-	largeArea = largePizzas * (PI * pow((LARGE_DIAMETER / 2), 2));
+    
+    
+    
+	smallArea = smallPizzas * (PI * pow((SMALL_RADIUS), SQUARE));
+	mediumArea = mediumPizzas * (PI * pow((MEDIUM_RADIUS), SQUARE));
+	largeArea = largePizzas * (PI * pow((LARGE_RADIUS), SQUARE));
 
 	totalArea = smallArea + mediumArea + largeArea;
 	areaPerPerson = totalArea / numOfGuests;
@@ -72,10 +75,10 @@ int main()
 	mediumTotal = mediumPizzas * MEDIUM_PRICE;
 	largeTotal = largePizzas * LARGE_PRICE;
 	totalBill = smallTotal + mediumTotal + largeTotal;
-	totalBill = (totalBill * (tipPercentage / 100)) + totalBill;
+	totalBill = (totalBill * (tipPercentage / CONVERT_TO_PERCENTAGE)) + totalBill;
 
 	//cout << "ST: " << smallTotal << "MT: " << mediumTotal << "LT: " << largeTotal;
-	cout << "\nThe total bill is: $" << fixed << setprecision(0) << totalBill << "\n\n";
+	cout << "\nThe total bill is: $" << fixed << setprecision(TO_NEAREST_DOLLAR) << totalBill << "\n\n";
 
 	system("pause");
 }
