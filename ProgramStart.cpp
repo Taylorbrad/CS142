a34cd003ad91a77f0d971b2fd386838de47eca7c
Taylor Bradshaw <quiero_videojuegos@hotmail.com>
2016-09-20 08:12:04 -0600

    Update and rename HeaderComment to ProgramStart.cpp

--- ProgramStart.cpp
+++ ProgramStart.cpp
@@ -1,18 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
 /*
 Taylor Bradshaw, Section _, taylor.bradshaw0250@gmail.com
 
 READY TO BE GRADED
 
-Purpose: 
+Description: 
 
 Inputs: 
 
 Outputs: 
 
 Test Case One:
+Inputs:
+Outputs:
 
 Test Case Two:
+Inputs:
+Outputs:
 
 Test Case Three:
-
+Inputs:
+Outputs:
 */
+
+int main()
+{
+
+return 0;
+}
