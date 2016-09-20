38b03731ba89074fe84f645e8e90e19971a42fb1
Taylor Bradshaw <quiero_videojuegos@hotmail.com>
2016-09-20 16:20:55 -0600

    Update Lab3.cpp

--- Lab3.cpp
+++ Lab3.cpp
@@ -34,171 +34,177 @@ Hybrid car better for gas consumption.
 */
 
 int main() {
-    
-    
-    const string nonHybridString = "Non-Hybrid Car";
-    const string hybridString = "Hybrid Car";
-    const string gasString = "gas";
-    const string costString = "total cost";
-    const int YEARS = 5;
-    double milesPerYear = 0;
-    double gasPricePerGallon = 0;
-    double efficiencyOfHybrid = 0;
-    double efficiencyOfNonHybrid = 0;
-    double costOfHybrid = 0;
-    double costOfNonHybrid = 0;
-    double resaleValueHybrid = 0;
-    double resaleValueNonHybrid = 0;
-    double totalGallonsHybrid = 0;
-    double totalGallonsNonHybrid = 0;
-    double costOfGasHybrid = 0;
-    double costOfGasNonHybrid = 0;
-    double depreciationHybrid = 0;
-    double depreciationNonHybrid = 0;
-    double totalCostHybrid = 0;
-    double totalCostNonHybrid = 0;
-    int confirmationInt = 0;
-    char userSelection = ' ';
-    string optionOne = "";
-    string optionTwo = "";
-    string optionThree = "";
-    
-    cout << "Estimated miles driven per year: ";
-    cin >> milesPerYear;
-    if (milesPerYear < 1)
-    {
-        cout << endl << "Please input a positive, non-zero number." << endl;
-        cout << "Estimated miles driven per year: ";
-        cin >> milesPerYear;
-    }
-    
-    cout << "Estimated price of gas per gallon: ";
-    cin >> gasPricePerGallon;
-    if (gasPricePerGallon < 1)
-    {
-        cout << endl << "Please input a positive, non-zero number." << endl;
-        cout << "Estimated price of gas per gallon: ";
-        cin >> gasPricePerGallon;
-    }
-    
-    cout << "Cost of hybrid car: ";
-    cin >> costOfHybrid;
-    if (costOfHybrid < 1)
-    {
-        cout << endl << "Please input a positive, non-zero number." << endl;
-        cout << "Cost of hybrid car: ";
-        cin >> costOfHybrid;
-    }
-    
-    cout << "Miles per gallon of hybrid car: ";
-    cin >> efficiencyOfHybrid;
-    if (efficiencyOfHybrid < 1)
-    {
-        cout << endl << "Please input a positive, non-zero number." << endl;
-        cout << "Miles per gallon of hybrid car: ";
-        cin >> efficiencyOfHybrid;
-    }
-    
-    cout << "Estimated resale value of hybrid car: ";
-    cin >> resaleValueHybrid;
-    if (resaleValueHybrid < 1)
-    {
-        cout << endl << "Please input a positive, non-zero number." << endl;
-        cout << "Estimated resale value of hybrid car: ";
-        cin >> resaleValueHybrid;
-    }
-    
-    cout << "Cost of regular car: ";
-    cin >> costOfNonHybrid;
-    if (costOfNonHybrid < 1)
-    {
-        cout << endl << "Please input a positive, non-zero number." << endl;
-        cout << "Cost of regular car: ";
-        cin >> costOfNonHybrid;
-    }
-    
-    cout << "Miles per gallon of regular car: ";
-    cin >> efficiencyOfNonHybrid;
-    if (efficiencyOfNonHybrid < 1)
-    {
-        cout << endl << "Please input a positive, non-zero number." << endl;
-        cout << "Miles per gallon of regular car: ";
-        cin >> efficiencyOfNonHybrid;
-    }
-    
-    cout << "Estimated resale value of regular car: ";
-    cin >> resaleValueNonHybrid;
-    if (resaleValueNonHybrid < 1)
-    {
-        cout << endl << "Please input a positive, non-zero number." << endl;
-        cout << "Estimated resale value of regular car: ";
-        cin >> resaleValueNonHybrid;
-    }
-    
-    //get total gallons and total cost for output
-    
-    totalGallonsHybrid = (milesPerYear * YEARS) / efficiencyOfHybrid;
-    totalGallonsNonHybrid = (milesPerYear * YEARS) / efficiencyOfNonHybrid;
-    
-    costOfGasHybrid = totalGallonsHybrid * gasPricePerGallon;
-    costOfGasNonHybrid = totalGallonsNonHybrid * gasPricePerGallon;
-    
-    depreciationHybrid = costOfHybrid - resaleValueHybrid;
-    depreciationNonHybrid = costOfNonHybrid - resaleValueNonHybrid;
-    
-    totalCostHybrid = depreciationHybrid + costOfGasHybrid;
-    totalCostNonHybrid = depreciationNonHybrid + costOfGasNonHybrid;
-    
-    cout << "Which is more important to you, gas consumption or total cost? (enter G for gas, T for total): ";
-    cin >> userSelection;
-    
-    
-    cout << "  --Hybrid Car Statistics--" << endl;
-    cout << "Total gas consumption over 5 years: " << fixed << setprecision(2) << totalGallonsHybrid << " gallons" << endl;
-    cout << "Total cost over 5 years: $" << totalCostHybrid << endl << endl;
-    cout << "  --Non Hybrid Car Statistics--" << endl;
-    cout << "Total gas consumption over 5 years: " << totalGallonsNonHybrid << " gallons" << endl;
-    cout << "Total cost over 5 years: $" << totalCostNonHybrid << endl << endl;
-    
-    if (toupper(userSelection) == toupper('g') && totalGallonsHybrid > totalGallonsNonHybrid)
-    {
-        optionOne = nonHybridString;
-        optionTwo = hybridString;
-        optionThree = gasString;
-        confirmationInt++;
-    }
-    else if (toupper(userSelection) == toupper('g') && totalGallonsHybrid < totalGallonsNonHybrid)
-    {
-        optionOne = hybridString;
-        optionTwo = nonHybridString;
-        optionThree = gasString;
-        confirmationInt++;
-    }
-    else if (toupper(userSelection) == toupper('g') && totalGallonsHybrid == totalGallonsNonHybrid)
-    {
-        cout << "These cars are equal in gas consumption over 5 years!";
-    }
-    else if (toupper(userSelection) == toupper('t') && totalCostHybrid > totalCostNonHybrid)
-    {
-        optionOne = nonHybridString;
-        optionTwo = hybridString;
-        optionThree = costString;
-        confirmationInt++;
-    }
-    else if (toupper(userSelection) == toupper('t') && totalCostHybrid > totalCostNonHybrid)
-    {
-        optionOne = hybridString;
-        optionTwo = nonHybridString;
-        optionThree = costString;
-        confirmationInt++;
-    }
-    else if (toupper(userSelection) == toupper('t') && totalCostHybrid == totalCostNonHybrid)
-    {
-        cout << "These cars have the same cost over 5 years!";
-    }
-    if (confirmationInt == 1)
-    {
-        cout << "The " << optionOne << " is better than the " << optionTwo << " When " << optionThree << " is the principle concern.";
-    }
-    
+
+
+	const string nonHybridString = "Non-Hybrid Car";
+	const string hybridString = "Hybrid Car";
+	const string gasString = "gas";
+	const string costString = "total cost";
+	const int YEARS = 5;
+	double milesPerYear = 0;
+	double gasPricePerGallon = 0;
+	double efficiencyOfHybrid = 0;
+	double efficiencyOfNonHybrid = 0;
+	double costOfHybrid = 0;
+	double costOfNonHybrid = 0;
+	double resaleValueHybrid = 0;
+	double resaleValueNonHybrid = 0;
+	double totalGallonsHybrid = 0;
+	double totalGallonsNonHybrid = 0;
+	double costOfGasHybrid = 0;
+	double costOfGasNonHybrid = 0;
+	double depreciationHybrid = 0;
+	double depreciationNonHybrid = 0;
+	double totalCostHybrid = 0;
+	double totalCostNonHybrid = 0;
+	int confirmationInt = 0;
+	string userSelection = "";
+	string optionOne = "";
+	string optionTwo = "";
+	string optionThree = "";
+
+	cout << "Estimated miles driven per year: ";
+	cin >> milesPerYear;
+	if (milesPerYear < 1)
+	{
+		cout << endl << "Please input a positive, non-zero number." << endl;
+		cout << "Estimated miles driven per year: ";
+		cin >> milesPerYear;
+	}
+
+	cout << "Estimated price of gas per gallon: ";
+	cin >> gasPricePerGallon;
+	if (gasPricePerGallon < 1)
+	{
+		cout << endl << "Please input a positive, non-zero number." << endl;
+		cout << "Estimated price of gas per gallon: ";
+		cin >> gasPricePerGallon;
+	}
+
+	cout << "Cost of hybrid car: ";
+	cin >> costOfHybrid;
+	if (costOfHybrid < 1)
+	{
+		cout << endl << "Please input a positive, non-zero number." << endl;
+		cout << "Cost of hybrid car: ";
+		cin >> costOfHybrid;
+	}
+
+	cout << "Miles per gallon of hybrid car: ";
+	cin >> efficiencyOfHybrid;
+	if (efficiencyOfHybrid < 1)
+	{
+		cout << endl << "Please input a positive, non-zero number." << endl;
+		cout << "Miles per gallon of hybrid car: ";
+		cin >> efficiencyOfHybrid;
+	}
+
+	cout << "Estimated resale value of hybrid car: ";
+	cin >> resaleValueHybrid;
+	if (resaleValueHybrid < 1)
+	{
+		cout << endl << "Please input a positive, non-zero number." << endl;
+		cout << "Estimated resale value of hybrid car: ";
+		cin >> resaleValueHybrid;
+	}
+
+	cout << "Cost of regular car: ";
+	cin >> costOfNonHybrid;
+	if (costOfNonHybrid < 1)
+	{
+		cout << endl << "Please input a positive, non-zero number." << endl;
+		cout << "Cost of regular car: ";
+		cin >> costOfNonHybrid;
+	}
+
+	cout << "Miles per gallon of regular car: ";
+	cin >> efficiencyOfNonHybrid;
+	if (efficiencyOfNonHybrid < 1)
+	{
+		cout << endl << "Please input a positive, non-zero number." << endl;
+		cout << "Miles per gallon of regular car: ";
+		cin >> efficiencyOfNonHybrid;
+	}
+
+	cout << "Estimated resale value of regular car: ";
+	cin >> resaleValueNonHybrid;
+	if (resaleValueNonHybrid < 1)
+	{
+		cout << endl << "Please input a positive, non-zero number." << endl;
+		cout << "Estimated resale value of regular car: ";
+		cin >> resaleValueNonHybrid;
+	}
+
+	//get total gallons and total cost for output
+
+	totalGallonsHybrid = (milesPerYear * YEARS) / efficiencyOfHybrid;
+	totalGallonsNonHybrid = (milesPerYear * YEARS) / efficiencyOfNonHybrid;
+
+	costOfGasHybrid = totalGallonsHybrid * gasPricePerGallon;
+	costOfGasNonHybrid = totalGallonsNonHybrid * gasPricePerGallon;
+
+	depreciationHybrid = costOfHybrid - resaleValueHybrid;
+	depreciationNonHybrid = costOfNonHybrid - resaleValueNonHybrid;
+
+	totalCostHybrid = depreciationHybrid + costOfGasHybrid;
+	totalCostNonHybrid = depreciationNonHybrid + costOfGasNonHybrid;
+
+	cout << "Which is more important, gas consumption or total cost? (Gas or Total): ";
+	cin >> userSelection;
+
+
+	cout << "  --Hybrid Car Statistics--" << endl;
+	cout << "Total gas consumption over 5 years: " << fixed << setprecision(2) << totalGallonsHybrid << " gallons" << endl;
+	cout << "Total cost over 5 years: $" << totalCostHybrid << endl << endl;
+	cout << "  --Non Hybrid Car Statistics--" << endl;
+	cout << "Total gas consumption over 5 years: " << totalGallonsNonHybrid << " gallons" << endl;
+	cout << "Total cost over 5 years: $" << totalCostNonHybrid << endl << endl;2
+
+	if (userSelection.compare("Gas") == 0 && totalGallonsHybrid > totalGallonsNonHybrid)
+	{
+		optionOne = nonHybridString;
+		optionTwo = hybridString;
+		optionThree = gasString;
+		confirmationInt++;
+	}
+	else if (userSelection.compare("Gas") == 0 && totalGallonsHybrid < totalGallonsNonHybrid)
+	{
+		optionOne = hybridString;
+		optionTwo = nonHybridString;
+		optionThree = gasString;
+		confirmationInt++;
+	}
+	else if (userSelection.compare("Gas") == 0 && totalGallonsHybrid == totalGallonsNonHybrid)
+	{
+		cout << "These cars are equal in gas consumption over 5 years!";
+	}
+	else if (userSelection.compare("Total") == 0 && totalCostHybrid > totalCostNonHybrid)
+	{
+		optionOne = nonHybridString;
+		optionTwo = hybridString;
+		optionThree = costString;
+		confirmationInt++;
+	}
+	else if (userSelection.compare("Total") == 0 && totalCostHybrid < totalCostNonHybrid)
+	{
+		optionOne = hybridString;
+		optionTwo = nonHybridString;
+		optionThree = costString;
+		confirmationInt++;
+	}
+	else if (userSelection.compare("Total") == 0 && totalCostHybrid == totalCostNonHybrid)
+	{
+		cout << "These cars have the same cost over 5 years!";
+	}
+	else
+	{
+		cout << "Please enter Gas or Total.";
+	}
+	if (confirmationInt == 1)
+	{
+		cout << "The " << optionOne << " is better than the " << optionTwo << " When " << optionThree << " is the principle concern.";
+	}
+	cout << endl << endl;
+	system("pause");
+
 }
