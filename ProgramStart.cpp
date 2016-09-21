dd26c22faf0df5bea2b0386901477e2661399300
Taylor Bradshaw <quiero_videojuegos@hotmail.com>
2016-09-21 08:59:32 -0600

    Update ProgramStart.cpp

--- ProgramStart.cpp
+++ ProgramStart.cpp
@@ -1,7 +1,3 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
 /*
 Taylor Bradshaw, Section _, taylor.bradshaw0250@gmail.com
 
@@ -25,9 +21,20 @@ Test Case Three:
 Inputs:
 Outputs:
 */
+#include <iostream>
+#include <string>
+using namespace std;
 
 int main()
 {
-//Program Here
+int selection = 0;
+
+
+cout << "Select an option:" << endl;
+cout << "0: Quit" << endl;
+cout << "1: Drop a single Plinko chip into one slot" << endl;
+cout << "2: Drop multiple Plinko chips into one slot" << endl << endl;
+cin >> selection;
+
 return 0;
 }
