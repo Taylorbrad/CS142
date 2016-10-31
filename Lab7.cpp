7a99cb64a3d055afd9c6fc90c310f723a4ed5645
Taylor Bradshaw <quiero_videojuegos@hotmail.com>
2016-10-31 11:36:51 -0600

    Update Lab7.cpp

--- Lab7.cpp
+++ Lab7.cpp
@@ -1,1 +1,36 @@
+// Example program
+#include <iostream>
+#include <string>
+#include <vector>
 
+using namespace std;
+
+
+int main()
+{
+    vector<string> restaurants;
+    int selection = 0;
+    
+    cin >> selection;
+    
+    switch(selection)
+    
+    case 1:
+    
+    break;
+    case 2:
+    
+    break;
+    case 3:
+    
+    break;
+    case 4:
+    
+    break;
+    case 5:
+    
+    break;
+    case 6:
+    
+    break;
+}
