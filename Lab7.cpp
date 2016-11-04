65e83975cab4c08aafd992cc42a24ad0570eb43b
Taylor Bradshaw <quiero_videojuegos@hotmail.com>
2016-11-04 13:16:26 -0600

    Update Lab7.cpp

--- Lab7.cpp
+++ Lab7.cpp
@@ -10,208 +10,247 @@ using namespace std;
 void menuSelection(int selection, vector<string>& restaurants);
 void displayRestaurants(const vector<string> restaurants);
 void addRestaurant(vector<string>& restaurants);
-void removeRestaurant(vector<string>& restaurants, string toFind);
+string removeRestaurant(vector<string>& restaurants, string toFind);
 void shuffleVector(vector<string>& restaurants);
-void tournament(vector<string>& restaurants);
-bool checkIfIsPower(int numToCheck);
+string tournament(vector<string>& restaurants);
+void incorrectInput();
+int checkIfIsPower(int numToCheck);
+int findRestaurant(vector<string>& restaurants, string toFind);
 
+const int ARBITRARY_ITERATION_NUMBER = 100;
+const int FALSE_RETURN_VALUE = -1;
+
+/*
+TEST CASES HOMIE
+TEST CASES HOMIE
+TEST CASES HOMIE
+TEST CASES HOMIE
+TEST CASES HOMIE
+TEST CASES HOMIE
+TEST CASES HOMIE
+TEST CASES HOMIE
+TEST CASES HOMIE
+TEST CASES HOMIE
+TEST CASES HOMIE
+TEST CASES HOMIE
+TEST CASES HOMIE
+TEST CASES HOMIE
+TEST CASES HOMIE
+TEST CASES HOMIE
+*/
 int main()
 {
-    vector<string> restaurants;
-    int selection = 0;
-    srand(time(0));
-    
-    restaurants.push_back("McDonald's");
-    restaurants.push_back("Little Caesar's");
-    restaurants.push_back("Arby's");
-    restaurants.push_back("Pizza Hut");
-    restaurants.push_back("Sonic");
-    restaurants.push_back("Winger's");
-    restaurants.push_back("Panda Express");
-    restaurants.push_back("In-N-Out Burger");
-    
-    while (selection < 5)
-    {
-        cout << endl << "1 - Display all restaurants";
-        cout << endl << "2 - Add a restaurant";
-        cout << endl << "3 - Remove a restaurant";
-        cout << endl << "4 - Shuffle the vector";
-        cout << endl << "5 - Begin the tournament";
-        cout << endl << "6 - Quit the program";
-        cout << endl << "Input: ";
-        cin >> selection;
-        
-        if (cin.fail())
-		{
-			cin.clear();
-			cin.ignore(1000, '\n');
-			cout << endl << "---Invalid Selection. Please select a number from the menu---" << endl;
+	vector<string> restaurants;
+	const int TOURNAMENT_OPTION = 5;
+	const int QUIT_OPTION = 6;
+	const int DISPLAY_OPTION = 1;
+	int selection = 0;
+	srand(time(0));
 
+	restaurants.push_back("McDonald's");
+	restaurants.push_back("Little Caesar's");
+	restaurants.push_back("Arby's");
+	restaurants.push_back("Pizza Hut");
+	restaurants.push_back("Sonic");
+	restaurants.push_back("Winger's");
+	restaurants.push_back("Panda Express");
+	restaurants.push_back("In-N-Out Burger");
+
+	while (selection < TOURNAMENT_OPTION || selection > QUIT_OPTION)
+	{
+		cout << endl << "1 - Display all restaurants";
+		cout << endl << "2 - Add a restaurant";
+		cout << endl << "3 - Remove a restaurant";
+		cout << endl << "4 - Shuffle the vector";
+		cout << endl << "5 - Begin the tournament";
+		cout << endl << "6 - Quit the program";
+		cout << endl << "Input: ";
+		cin >> selection;
+
+		if (cin.fail() || selection > QUIT_OPTION || selection < DISPLAY_OPTION)
+		{
+			incorrectInput();
 		}
 		else
 		{
-		    menuSelection(selection, restaurants);
+			menuSelection(selection, restaurants);
 		}
-        
-    }
-    
+	}
+
 }
 void menuSelection(int selection, vector<string>& restaurants)
 {
-    string toFind = "";
-    
-    switch(selection)
-    {
-    case 1:
-    displayRestaurants(restaurants);
-    break;
-    case 2:
-    addRestaurant(restaurants);
-    break;
-    case 3:
-    
-    cout << "Which restaurant would you like to remove? :";
-    cin.ignore();
-    getline(cin, toFind);
-    
-    removeRestaurant(restaurants, toFind);
-    break;
-    case 4:
-    shuffleVector(restaurants);
-    break;
-    case 5:
-    tournament(restaurants);
-    break;
-    case 6:
-    //DONE
-    cout << endl << "--Exiting--" << endl;
-    system("pause");
-    break;
-    }
+	string toFind = "";
+
+	switch (selection)
+	{
+	case 1:
+		displayRestaurants(restaurants);
+		break;
+	case 2:
+		addRestaurant(restaurants);
+		break;
+	case 3:
+		cout << "Which restaurant would you like to remove?: ";
+		cin.ignore();
+		getline(cin, toFind);
+
+		cout << removeRestaurant(restaurants, toFind) << endl;
+		break;
+	case 4:
+		shuffleVector(restaurants);
+		break;
+	case 5:
+		cout << tournament(restaurants) << " is the winner!!";
+		system("pause");
+		break;
+	case 6:
+		cout << endl << "--Exiting--" << endl;
+		system("pause");
+		break;
+	}
 }
 void displayRestaurants(const vector<string> restaurants)
 {
-    for (int length = 0; length < restaurants.size(); ++length)
-    {
-        cout << restaurants[length];
-        
-        if (!(length + 1 == restaurants.size()))
-        {
-            cout << ", ";
-        }
-    }
+	for (int length = 0; length < restaurants.size(); ++length)
+	{
+		cout << restaurants[length];
+
+		if (!(length + 1 == restaurants.size()))
+		{
+			cout << ", ";
+		}
+	}
 }
 void addRestaurant(vector<string>& restaurants)
 {
-    string newRestaurant;
-    
-    cout << "Restaurant name: ";
-    cin.ignore();
-    getline(cin, newRestaurant);
-    //cin >> newRestaurant;
-    
-    if (find(restaurants.begin(), restaurants.end(), newRestaurant) != restaurants.end())
-    {
-        cout << "'" << newRestaurant <<"' already exists.";
-    }
-    else
-    {
-        restaurants.push_back(newRestaurant);
-        cout << "'" << newRestaurant << "' added to restaurants." << endl;
-    }
+	string newRestaurant;
+
+	cout << "Restaurant name: ";
+	cin.ignore();
+	getline(cin, newRestaurant);
+
+	if (findRestaurant(restaurants, newRestaurant) != FALSE_RETURN_VALUE)
+	{
+		cout << "'" << newRestaurant << "' already exists." << endl;
+	}
+	else
+	{
+		restaurants.push_back(newRestaurant);
+		cout << "'" << newRestaurant << "' added to restaurants." << endl;
+	}
 }
-void removeRestaurant(vector<string>& restaurants, string toFind)
+string removeRestaurant(vector<string>& restaurants, string toFind)
 {
-    if (find(restaurants.begin(), restaurants.end(), toFind) != restaurants.end())
-    {
-        //Erase the "removed" elements.
-        restaurants.erase(find(restaurants.begin(), restaurants.end(), toFind));
-        cout << "'" << toFind << "' removed from restaurants." << endl;
-    }
-    else
-    {
-        cout << "'" << toFind << "' not found" << endl;
-    }
+	string removedRestaurant = "Restaurant not found.";
+	int restaurantSubscript = 0;
+	restaurantSubscript = findRestaurant(restaurants, toFind);
+
+	if (restaurantSubscript != FALSE_RETURN_VALUE)
+	{
+		restaurants.erase(restaurants.begin() + restaurantSubscript);
+		removedRestaurant = "'" + toFind + "' Removed.";
+	}
+	return removedRestaurant;
 }
 void shuffleVector(vector<string>& restaurants)
 {
-    int randomSubscriptOne = 0;
-    int randomSubscriptTwo = 0;
-    string tempString = "";
-    
-    for (int i = 0; i < 100; ++i)
-    {
-        randomSubscriptOne = rand() % restaurants.size();
-        randomSubscriptTwo = rand() % restaurants.size();
-        
-        tempString = restaurants[randomSubscriptOne];
-        restaurants[randomSubscriptOne] = restaurants[randomSubscriptTwo];
-        restaurants[randomSubscriptTwo] = tempString;
-    }
-    cout << endl << "List has been shuffled!";
-}
-void tournament(vector<string>& restaurants)
-{
-    int selection = 0;
-    int halfOfTheRestaurants = restaurants.size() / 2;
-    int size = restaurants.size();
 
-    if (checkIfIsPower(size))
-    {
-        for (int i = 1; size > 1; ++i)
-        {
-            //cout << i;
-            cout << "#1: " << restaurants[i - 1] << endl;
-            cout << "#2: " << restaurants[i] << endl;
-            cout << "Which restaurant do you prefer? (1 or 2): ";
-            cin >> selection;
-            //cout << "nofail";
-            //selection = 1;
-            if (cin.fail() || selection < 1 || selection > 3)
-    		{
-    		    if (cin.fail())
-    			{
-            	    cin.clear();
-        	        cin.ignore(1000, '\n');
-    		    }
-    			cout << endl << "---Invalid Selection. Please select a number from the menu---" << endl;
-    			--i;
-    		}
-    		else
-    		{
-    		    switch (selection)
-    		    {
-    		        case 1:
-    		            removeRestaurant(restaurants, restaurants[i]);
-    		            --size;
-    		            
-    		        break;
-    		        case 2:
-    		            removeRestaurant(restaurants, restaurants[i - 1]);
-    		            --size;
-    		        break;
-    		    }
-    		}
-    		if (size == halfOfTheRestaurants)
-    		{
-    		    i = 0;
-    		    halfOfTheRestaurants = restaurants.size() / 2;
-    		}
-        }
-        cout << restaurants[0] << " is the winner!!" << endl;
-    }
-    else
-    {
-        cout << "Incorrect amount of opponents!" << endl;
-    }
+	int randomSubscriptOne = 0;
+	int randomSubscriptTwo = 0;
+	string tempString = "";
+
+	for (int i = 0; i < ARBITRARY_ITERATION_NUMBER; ++i)
+	{
+		randomSubscriptOne = rand() % restaurants.size();
+		randomSubscriptTwo = rand() % restaurants.size();
+
+		tempString = restaurants[randomSubscriptOne];
+		restaurants[randomSubscriptOne] = restaurants[randomSubscriptTwo];
+		restaurants[randomSubscriptTwo] = tempString;
+	}
+	cout << endl << "List has been shuffled!" << endl;
 }
-bool checkIfIsPower(int numberToCheck)
+string tournament(vector<string>& restaurants)
 {
-    for (int power = 1; power < 100; ++power)
-    {
-        if (numberToCheck == pow(2, power))
-        {
-            return true;
-        }
-    }
+	const int OPTION_ONE = 1;
+	const int OPTION_TWO = 2;
+	int selection = 0;
+	int halfOfTheRestaurants = restaurants.size() / 2;
+	int size = restaurants.size();
+	int roundIterator = 1;
+	int power = checkIfIsPower(size);
+
+	if (power != FALSE_RETURN_VALUE)
+	{
+		for (int i = 1; size > 1; ++i)
+		{
+			cout << endl << "Which restaurant do you prefer?" << endl;
+			cout << "Match " << i << "/" << halfOfTheRestaurants << " Round " << roundIterator << "/" << power << endl;
+			cout << "#1: " << restaurants[i - 1] << endl;
+			cout << "#2: " << restaurants[i] << endl << "Input: ";
+			cin >> selection;
+
+			if (cin.fail() || selection < OPTION_ONE || selection > OPTION_TWO)
+			{
+				incorrectInput();
+				--i;
+			}
+			else if (selection == 1)
+			{
+				removeRestaurant(restaurants, restaurants[i]);
+				--size;
+			}
+			else if (selection == 2)
+			{
+				removeRestaurant(restaurants, restaurants[i - 1]);
+				--size;
+			}
+
+			if (size == halfOfTheRestaurants)
+			{
+				i = 0;
+				halfOfTheRestaurants = restaurants.size() / 2;
+				++roundIterator;
+			}
+		}
+		return restaurants[0];
+	}
+	else
+	{
+		cout << "Incorrect amount of opponents!" << endl;
+		main();
+	}
+}
+int checkIfIsPower(int numberToCheck)
+{
+	for (int power = 1; power < ARBITRARY_ITERATION_NUMBER; ++power)
+	{
+		if (numberToCheck == pow(2, power))
+		{
+			return power;
+		}
+	}
+	return -1;
+}
+int findRestaurant(vector<string>& restaurants, string toFind)
+{
+	const int BEGINNING_OF_LIST = -1;
+	for (int i = restaurants.size() - 1; i > BEGINNING_OF_LIST; --i)
+	{
+		if (toFind == restaurants[i])
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+void incorrectInput()
+{
+	if (cin.fail())
+	{
+		cin.clear();
+		cin.ignore(1000, '\n');
+	}
+	cout << endl << "---Invalid Selection. Please select a number from the menu---" << endl;
 }
