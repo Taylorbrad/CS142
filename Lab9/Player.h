30de13904ea5b1d30bb348d82ffd112c1dfdbb99
Taylor Bradshaw <quiero_videojuegos@hotmail.com>
2016-11-15 15:55:53 -0700

    Update Player.h

--- Lab9/Player.h
+++ Lab9/Player.h
@@ -1,1 +1,18 @@
 
+#include <iostream>
+#include <vector>
+#include <string>
+//#include <>
+
+class Player
+{
+    private:
+            string name;
+            
+    public:
+            //Constructor for player
+            Player(string nameInput);
+            /*Figure out what this does*/virtual ~Player();
+            
+            //function definitios here
+            
