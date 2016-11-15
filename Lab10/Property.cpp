e83756ddc6e6d85d02b10f2e84c6694fc4c14833
Taylor Bradshaw <quiero_videojuegos@hotmail.com>
2016-11-15 16:17:45 -0700

    Update Property.cpp

--- Lab10/Property.cpp
+++ Lab10/Property.cpp
@@ -1,1 +1,18 @@
+// Example program
+#include <iostream>
+#include <string>
+#include <sstream>
+#include "Property.h"
+using namespace std;
 
+Property::Property(string addressInput)
+{
+    address = addressInput;
+}
+
+Property::toString()
+{
+    stringstream ss;
+    ss << "Address: " << address;
+    return ss.str();
+}
