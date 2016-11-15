1fe35bce64a9360f5f8e66e620ac5292b3e27dbb
Taylor Bradshaw <quiero_videojuegos@hotmail.com>
2016-11-15 15:56:53 -0700

    Update Lab9main.cpp

--- Lab9/Lab9main.cpp
+++ Lab9/Lab9main.cpp
@@ -1,1 +1,21 @@
 
+#include <vector>
+#include <iostream>
+#include <string>
+#include "Player.h"
+
+int main()
+{
+    int selection = 0;
+    vector<Player*> listOfPlayers;
+    vector<Player*> lineUpOfPlayers;
+    
+    cout << "1 - Show Players" << endl;
+    cout << "2 - Add Player" << endl;
+    cout << "3 - Add to Line-Up" << endl;
+    cout << "4 - Show Line-Up" << endl;
+    cout << "5 - Fight!" << endl;
+    cout << "6 - Quit" << endl;
+    
+    
+}
