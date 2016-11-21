d7f3f3cc6c49437204218e88b6389b7356befec6
Taylor Bradshaw <quiero_videojuegos@hotmail.com>
2016-11-21 16:21:24 -0700

    Update Player.h

--- Lab9/Player.h
+++ Lab9/Player.h
@@ -13,15 +13,19 @@ class Player
             int losses = 0;
             int draws = 0;
             int games = 0;
+            int rpsThrow = 0;
             double record = wins / games;
             
     public:
             //Constructor for player
             Player(string nameInput);
             calculateRecord();
-            getRPSThrow();
+            getRPSThrow(int randNumb);
             toString();
+            addWin();
+            addLoss();
+            addDraw();
             /*Figure out what this does*/virtual ~Player();
             
             //function definitios here
-            
+};
