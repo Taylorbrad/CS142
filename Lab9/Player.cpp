d0454f582dc4f7923cac81554d2832b83b1df66e
Taylor Bradshaw <quiero_videojuegos@hotmail.com>
2016-11-21 14:19:41 -0700

    Update Player.cpp

--- Lab9/Player.cpp
+++ Lab9/Player.cpp
@@ -14,25 +14,5 @@ void Player::calculateRecord()
 {
     this->record = this->wins / this->games;
 }
-string Player::displayPlayers(vector<Player*> allPlayers)
-{
-    stringstream ss;
-    ss << endl;
-    ss << "--------------------------------";
-    calculateRecord();
-    for (int i = 0; i < allPlayers.size(); ++i)
-    {
-        if (i != 0)
-        {
-        ss << "-----------------";
-        }
-        ss << "#" << i + 1 << ": " << endl;
-        ss << (*(allPlayers[i])).name << endl;
-        ss << "Wins: " << (*(allPlayers[i])).wins << endl;
-        ss << "Losses: " << (*(allPlayers[i])).losses << endl;
-        ss << "Draws: " << (*(allPlayers[i])).draws << endl;
-        ss << "Record: " << (*(allPlayers[i])).record << endl;
-    }
-    ss << "--------------------------------" << endl << endl;
-}
+
 Player::~Player(){}
