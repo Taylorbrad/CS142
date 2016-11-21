0ad72db8cc4f77e4f432653ef135bfbb97557efa
Taylor Bradshaw <quiero_videojuegos@hotmail.com>
2016-11-21 13:50:30 -0700

    Update Player.cpp

--- Lab9/Player.cpp
+++ Lab9/Player.cpp
@@ -1,4 +1,8 @@
 #include "Player.h"
+#include <vector>
+#include <iostream>
+#include <string>
+#include <sstream>
 using namespace std;
 
 
@@ -6,5 +10,29 @@ Player::Player(string nameInput)
 {
     name = nameInput;
 }
+void Player::calculateRecord()
+{
+    this->record = this->wins / this->games;
+}
+string Player::displayPlayers(vector<Player*> allPlayers)
+{
+    stringstream ss;
+    ss << endl;
+    ss << "--------------------------------";
+    calculateRecord();
+    for (int i = 0; i < allPlayers.size(); ++i)
+    {
+        if (i != 0)
+        {
+        ss << "-----------------";
+        }
+        ss << "#" << i + 1 << ": " << endl;
+        ss << (*(allPlayers[i])).name << endl;
+        ss << "Wins: " << (*(allPlayers[i])).wins << endl;
+        ss << "Losses: " << (*(allPlayers[i])).losses << endl;
+        ss << "Draws: " << (*(allPlayers[i])).draws << endl;
+        ss << "Record: " << (*(allPlayers[i])).record << endl;
+    }
+    ss << "--------------------------------" << endl << endl;
+}
 Player::~Player(){}
-
