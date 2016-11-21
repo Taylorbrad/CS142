92994a3bd91312514870bcdd1ed9f53be93d332c
Taylor Bradshaw <quiero_videojuegos@hotmail.com>
2016-11-21 13:50:47 -0700

    Update Player.h

--- Lab9/Player.h
+++ Lab9/Player.h
@@ -9,10 +9,17 @@ class Player
 {
     private:
             string name;
+            int wins = 0;
+            int losses = 0;
+            int draws = 0;
+            int games = 0;
+            double record = wins / games;
             
     public:
             //Constructor for player
             Player(string nameInput);
+            calculateRecord();
+            displayPlayers(vector<Player*> allPlayers);
             /*Figure out what this does*/virtual ~Player();
             
             //function definitios here
