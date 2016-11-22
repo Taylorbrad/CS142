c920e5175597d66b9c4550eb2af2c3766634092a
Taylor Bradshaw <quiero_videojuegos@hotmail.com>
2016-11-21 23:35:16 -0700

    Update Player.h

--- Lab9/Player.h
+++ Lab9/Player.h
@@ -1,31 +1,29 @@
-#pragma once //Figure out what this is also
-#include <iostream>
-#include <vector>
 #include <string>
 //#include <>
 using namespace std;
 
 class Player
 {
-    private:
-            string name;
-            int wins = 0;
-            int losses = 0;
-            int draws = 0;
-            int games = 0;
-            int rpsThrow = 0;
-            double record = wins / games;
-            
-    public:
-            //Constructor for player
-            Player(string nameInput);
-            calculateRecord();
-            getRPSThrow(int randNumb);
-            toString();
-            addWin();
-            addLoss();
-            addDraw();
-            /*Figure out what this does*/virtual ~Player();
-            
-            //function definitios here
+private:
+	string name;
+	int wins = 0;
+	int losses = 0;
+	int draws = 0;
+	int games = 0;
+	int rpsThrow = 0;
+	double record = 0;
+
+public:
+	//Constructor for player
+	Player(string nameInput);
+	void calculateRecord();
+	string getRPSThrow(int randNumb);
+	string toString();
+	void addWin();
+	void addLoss();
+	void addDraw();
+	string getName();
+	/*Figure out what this does*/virtual ~Player();
+
+	//function definitios here
 };
