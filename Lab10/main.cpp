39a77aa150603277a00273f29ad4e8631d308467
Taylor Bradshaw <quiero_videojuegos@hotmail.com>
2016-11-28 12:32:50 -0700

    Update main.cpp

--- Lab10/main.cpp
+++ Lab10/main.cpp
@@ -1,14 +1,16 @@
-
+// Example program
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <sstream>
+#include <vector>
+
 using namespace std;
-
-
 string getFileName()
 {
     string fileName = 0;
     
-    cout << "Input file name (with the extension): ";
+    cout << "Where should I read the data from? ";
     cin >> fileName;
     
     return fileName;
@@ -16,6 +18,19 @@ string getFileName()
 int main()
 {
     string fileName = "";
+    ifstream toOpen;
     
     fileName = getFileName();
+    
+    toOpen.open("fileName");
+    
+    if (toOpen.is_open();
+    {
+        
+    }
+    else
+    {
+        cout << "There was an error reading the file!";
+    }
+    
 }
