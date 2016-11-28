7e9a7bdd903718e47533cf9216e243a08b386bff
Taylor Bradshaw <quiero_videojuegos@hotmail.com>
2016-11-28 12:26:35 -0700

    Update main.cpp

--- Lab10/main.cpp
+++ Lab10/main.cpp
@@ -1,1 +1,21 @@
 
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+string getFileName()
+{
+    string fileName = 0;
+    
+    cout << "Input file name (with the extension): ";
+    cin >> fileName;
+    
+    return fileName;
+}
+int main()
+{
+    string fileName = "";
+    
+    fileName = getFileName();
+}
