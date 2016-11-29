21a2c9b5c8dba7d87f8567f75bbeaa8b4adbfc20
Taylor Bradshaw <quiero_videojuegos@hotmail.com>
2016-11-29 13:56:28 -0700

    Update Commercial.h

--- Lab10/Commercial.h
+++ Lab10/Commercial.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -5,3 +6,11 @@
 #include <vector>
 #include "Property.h"
 using namespace std;
+
+class Commercial : public Property
+{
+    public:
+    
+    private:
+    
+};
