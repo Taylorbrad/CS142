928832de2e63aa24ef5b95580ada0b301b982e49
Taylor Bradshaw <quiero_videojuegos@hotmail.com>
2016-11-29 16:09:10 -0700

    Update main.cpp

--- Lab10/main.cpp
+++ Lab10/main.cpp
@@ -19,47 +19,70 @@ int main()
 {
     string fileName = "";
     ifstream toOpen;
+    ofstream toWrite;
+    stringstream ss;
+    stringstream ss1;
     string address = "";
     string typeOfProperty = "";
     double value = 0;
     double discount = 0;
-    int id = 0;
     bool isRental = false;
     bool isDiscounted = false;
     bool isOccupied = false;
+    vector<Commercial*> commercialProperties;
+    vector<Residential*> residentialProperties;
+    vector<stringstream> badLinesSS;
     
     fileName = getFileName();
     
-    toOpen.open("fileName");
+    toOpen.open((fileName + ".txt"));
     
     if (toOpen.is_open());
     {
-        while (!toOpen.eof())
+        for (int id = 0; !toOpen.eof(); ++id)
         {
+            getline(toOpen, ss, '\n') //put the entire line in ss
+                /*TODO: try pulling from ss instead of toOpen in the following lines */
             getline(toOpen, typeOfProperty, ' ');
             getline(toOpen, isRental, ' ');
             getline(toOpen, value, ' ');
             if (typeOfProperty = "Commercial")
-                {
-                    getline(toOpen, isDiscounted, ' ');
-                    getline(toOpen, discount, ' ');
-                    getline(toOpen, address, '\n');
-                }
+            {
+                
+                getline(toOpen, isDiscounted, ' ');
+                getline(toOpen, discount, ' ');
+                getline(toOpen, address, '\n');
+                    
+                Commercial* test = new Commercial(address, isDiscounted,  isRental, discount, value, id);
+                commercialProperties.push_back(test); 
+            }
             else if (typeOfProperty = "Residential")
-                {
-                    
-                    getline(toOpen, isOccupied, ' ');
-                    getline(toOpen, address, '\n');
-                }
+            {
+                getline(toOpen, isOccupied, ' ');
+                getline(toOpen, address, '\n');
+                
+                Residential* test1 = new Residential(/*TODO: add Paramaters*/);
+                residentialProperties.push_back(test1);
+            }
             else
-                {
-                    
-                }
+            {
+                badLinesSS.push_back(ss);
+            }
         }
     }
     else
     {
         cout << "There was an error reading the file!";
     }
+    //TODO: Open new file for writing to, and write the necessary output.
+    toWrite.open((fileName + "OUT.txt"));
     
+    if (toWrite.is_open())
+    {
+        
+    }
+    else
+    {
+        cout << "There was an error reading the file!";
+    }
 }
