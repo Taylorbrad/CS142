5b9c1064b3d5917dab77708afbab1e402e92ede2
Taylor Bradshaw <quiero_videojuegos@hotmail.com>
2016-11-29 16:14:15 -0700

    Update Residential.h

--- Lab10/Residential.h
+++ Lab10/Residential.h
@@ -9,6 +9,8 @@ using namespace std;
 class Residential : public Property
 {
     public:
+    Residential(string address, bool isOccupied, bool isRental, double value, int id) : Property(address, isRental, value, id);
+    string toString();
     
     private:
     bool isOccupied;
